d7931daa46d4b9e33831b69ad09f860666db87ed
Hormat Omar <hormatomar@gmail.com>
2022-03-24 20:49:09 -0400

    "now"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        for(int i=0; i<10 ;i++) {
+        for(int i=0; i<5 ;i++) {
             System.out.println("Batch 11 is funny and great and I miss you guys");
             Thread.sleep(2000);
-            
+
 
         }
 
