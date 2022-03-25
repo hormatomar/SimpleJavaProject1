5b207af96ff83cf9242cfe814816c6cd99f17937
Hormat Omar <hormatomar@gmail.com>
2022-03-24 20:24:26 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        for(int i=0; i<10 ;i++) {
+            System.out.println("Batch 11 is funny and great and I miss you guys");
+            Thread.sleep(2000);
+            
+
+        }
+
+    }
+}
